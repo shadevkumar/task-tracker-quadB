fba44b284ec5981d33648da8887e02bc38e01c91
shadevkumar <shadevsrk261@gmail.com>
2024-04-01 14:55:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex bg-[#161616] justify-center items-center min-h-screen text-white ">
+        Task Tracker QuadB
+      </div>
+    </>
+  );
+}
+
+export default App;
