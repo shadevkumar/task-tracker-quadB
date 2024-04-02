309a1cd9ed1dd9d0c2860fba51b3e8cd0b40ea13
shadevkumar <shadevsrk261@gmail.com>
2024-04-02 05:30:46 +0530

    Mobile version responsive iteration completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import AllTasks from "./components/AllTasks";
+import { useSelector } from "react-redux";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const showAddItemCard = useSelector((state) => state.addItemCard.flag);
   return (
     <>
-      <div className="flex bg-[#161616] min-h-screen text-white ">
+      <div
+        className={` flex max-md:flex-col bg-[#161616] min-h-screen text-white ${
+          showAddItemCard && "blur-md"
+        } `}
+      >
         <Sidebar />
         <AllTasks />
       </div>
+      {showAddItemCard && <AddTask />}
     </>
   );
 }
--- src/components/AllTasks.jsx
+++ src/components/AllTasks.jsx
@@ -1,21 +1,22 @@
-import { useSelector } from "react-redux";
-import AddTask from "./AddTask";
 import TaskCardsView from "./TaskCardsView";
 import TaskListView from "./TaskListView";
 import { useState } from "react";
 import { FaList } from "react-icons/fa";
 import { BsCardHeading } from "react-icons/bs";
 // import ListIcon from '@mui/icons-material/List';
+import { FaPlus } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { addItemCard } from "../features/addItemCard/addItemCardSlice";
 
 const AllTasks = () => {
-  const displayAddTask = useSelector((state) => state.addItemCard.flag);
+  const dispatch = useDispatch();
 
-  const [view, setView] = useState("cards"); // Default to cards view
+  const [view, setView] = useState("list"); // Default to cards view
 
   return (
     <>
-      <div className="md:w-10/12">
-        <div className="mt-4 mr-2 px-9 py-2 w-full flex justify-end ">
+      <div className="md:w-10/12 scrollbar-hide ">
+        <div className="mt-4 mr-2 max-md:hidden px-9 py-2 w-full flex justify-end ">
           <FaList
             onClick={() => {
               setView("list");
@@ -29,8 +30,16 @@ const AllTasks = () => {
             className="mx-2 cursor-pointer"
           />
         </div>
-        {view === "cards" ? <TaskCardsView /> : <TaskListView />}
-        {displayAddTask && <AddTask />}
+        {view === "list" ? <TaskListView /> : <TaskCardsView />}
+
+        <div
+          onClick={() => {
+            dispatch(addItemCard(true));
+          }}
+          className="md:hidden w-16 h-16 flex justify-center items-center fixed bottom-10 right-4 z-50   px-2 py-2 text-center font-semibold border-[1px] border-[#1B1B1B] bg-[#292929] text-[#d5d5d5] rounded-full hover:bg-[#2e2e2e] cursor-pointer text-3xl shadow-lg shadow-grey-500/40 "
+        >
+          <FaPlus className="" />
+        </div>
       </div>
     </>
   );
